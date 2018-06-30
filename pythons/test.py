7c367f1b7e69f78524b87635bbc0b55a8000ec23
Ghanshyam <ghanshyam@naukri.com>
2018-06-30 11:15:48 +0530

    keystroke data for all

--- pythons/test.py
+++ pythons/test.py
@@ -1,17 +1,14 @@
 import numpy as np
 from scipy.spatial import distance
+
+
 np.set_printoptions(suppress=True)
 newInp = np.loadtxt("current_attempt.txt", dtype='f', delimiter=',')
 training = np.loadtxt("training_data.txt", dtype='f', delimiter=',')
 
-#tracov= np.cov(training.T)
-#incov = np.linalg.inv(tracov)
-
 meantrain= training.mean(axis=0)
 
-#print distance.mahalanobis(meantrain,newInp,incov)
-print meantrain
-print "euclidean {}".format(distance.euclidean(newInp,meantrain))
-#delta = meantrain - newInp
-#m = np.dot(np.dot(delta, incov), delta)
-#np.sqrt(m*-1)
+for row in newInp:
+  print "euclidean {}".format(distance.euclidean(row,meantrain))
+
+

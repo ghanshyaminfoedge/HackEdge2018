01c72052253bc53a1e69874d3bff11a231bcd67a
Ghanshyam <ghanshyam@naukri.com>
2018-06-30 22:54:17 +0530

    weighted euclidean

--- pythons/app.py
+++ pythons/app.py
@@ -3,37 +3,64 @@ from flask import Flask
 import numpy as np
 from scipy.spatial import distance
 from flask import request
+import sklearn
+from sklearn.cluster import DBSCAN
+import math
 
 app = Flask(__name__)
 
 
-def mahalanobis(userName,keyPass):
-	print "{} {}".format(userName,keyPass)
-	np.set_printoptions(suppress=True)
-	newInp = np.loadtxt("/data/current_attempt_"+userName+"_"+keyPass+".txt", dtype='f', delimiter=',')
-	training = np.loadtxt("/data/training_"+userName+"_"+keyPass+".txt", dtype='f', delimiter=',')
-	np.absolute(newInp)
-	np.absolute(training)
-	meantrain= training.mean(axis=0)
-	cov = np.cov(training.T)
-	invcov = np.linalg.inv(cov)
-	row = newInp	
-	u = distance._validate_vector(row)
-	v = distance._validate_vector(meantrain)
-	VI = np.atleast_2d(invcov)
-	delta = u - v
-	m = np.dot(np.dot(delta, VI), delta)
-	m = np.absolute(m)
-	result = np.sqrt(m)
-	# number of keystroke
-	normalizedRes = training.shape[0]/3 
-	maxScore = 5000
-	percentage = (normalizedRes*100)/maxScore
-	return (100-percentage)
+def euclieanDis(currentfile, trainfile):
+        np.set_printoptions(suppress=True)
+        newInp = np.loadtxt(currentfile, dtype='f', delimiter=',')
+        training = np.loadtxt(trainfile, dtype='f', delimiter=',')
+
+        np.absolute(newInp)
+        np.absolute(training)
+        meantrain= training.mean(axis=0)
+        meansize=(meantrain.shape)[0]/3
+        weight = list()
+        for i in range (meansize):
+                weight.append(50)
+                weight.append(20)
+                weight.append(40)
+        weight.append(50)
+
+        we = np.asarray(weight)
+
+        res = list()
+        for row in newInp:
+                curres = distance.euclidean(row,meantrain,we);
+                res.append(curres)
+        return res
+
+
+def isValid(username,keypass):
+	res = euclieanDis("/data/training_"+username+"_"+keypass+".txt","/data/training_"+username+"_"+keypass+".txt")
+	dbinput = np.asarray(res)
+	dbinput = [[x,1] for x in dbinput]
+	stdv = math.ceil(np.std(dbinput))
+	model  = DBSCAN(eps=int(stdv), min_samples=3).fit(dbinput)
+	clust = list()
+	i=0
+	for row in model.labels_:
+	        if row == 0 :
+	                clust.append(res[i])
+	        i=i+1
+	resnew= euclieanDis("/data/current_"+username+"_"+keypass+".txt","/data/training_"+username+"_"+keypass+".txt")
+	newstd=np.std(np.asarray(clust))
+	newmean=np.mean(np.asarray(clust))
+	print "resnew {} newstd {} newmean {}".format(resnew,newstd,newmean)
+	thresoldpos = newmean+newstd*2 
+	thresoldnew = newmean-newstd*2
+	if np.mean(resnew) > thresoldpos or np.mean(resnew) < thresoldnew:
+		return 0
+	return 1
+
 
 @app.route('/keystroke/api/score', methods=['GET'])
 def getScore():
-	res = mahalanobis(request.args.get('userName'), request.args.get('keyPass'))
+	res = isValid(request.args.get('userName'), request.args.get('keyPass'))
 	print "result ============ {}".format(res)
 	return str(int(res))
 

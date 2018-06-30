615a12552e572acc9c2fff2bab8d220a822d890c
Ghanshyam <ghanshyam@naukri.com>
2018-06-30 10:44:55 +0530

    keystroke data for test123

--- pythons/test.py
+++ pythons/test.py
@@ -4,14 +4,14 @@ np.set_printoptions(suppress=True)
 newInp = np.loadtxt("current_attempt.txt", dtype='f', delimiter=',')
 training = np.loadtxt("training_data.txt", dtype='f', delimiter=',')
 
-tracov= np.cov(training.T)
-incov = np.linalg.inv(tracov)
+#tracov= np.cov(training.T)
+#incov = np.linalg.inv(tracov)
 
 meantrain= training.mean(axis=0)
 
 #print distance.mahalanobis(meantrain,newInp,incov)
 print meantrain
 print "euclidean {}".format(distance.euclidean(newInp,meantrain))
-delta = meantrain - newInp
-m = np.dot(np.dot(delta, incov), delta)
-print np.sqrt(m*-1)
+#delta = meantrain - newInp
+#m = np.dot(np.dot(delta, incov), delta)
+#np.sqrt(m*-1)

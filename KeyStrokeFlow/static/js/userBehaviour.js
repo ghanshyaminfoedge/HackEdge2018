e46a2475e7c98a022dfe4b493d3c5498d5e50b17
sonakshi.jain <sonakshi.jain@naukri.com>
2018-06-30 23:02:42 +0530

    integerating UBA with login and boat detection

---
+++ KeyStrokeFlow/static/js/userBehaviour.js
@@ -0,0 +1,66 @@
+furthestScrollPosition = 0;
+clickCount = 0;
+
+window.onload = function() {
+	pageEntryTime = new Date();
+}
+
+window.onscroll = function() {
+	var element = document.documentElement;
+	if(element.scrollTop > furthestScrollPosition) 
+		furthestScrollPosition = element.scrollTop;
+	else
+		finalScrollPosition = element.scrollTop;
+}
+
+window.onclick = function() {
+	clickCount++;
+}
+
+function generateDumpString() {
+	timeOnPage = (new Date()) - pageEntryTime;
+
+	
+	str = "{";
+	str = str + "\"url\":\"" + window.location.href + "\",";
+	str = str + "\"timeOnPage\":\"" + timeOnPage + "\",";
+	str = str + "\"furthestScrollPosition\":\"" + furthestScrollPosition + "\",";
+	str = str + "\"finalScrollPosition\":\"" + finalScrollPosition + "\"";
+	str = str + "\"clickCount\":\"" + clickCount + "\"";
+	str = str + "}";
+	return str;
+}
+
+function postBlog() {
+	timeOnPage = (new Date()) - pageEntryTime;
+	 var request = $.ajax({
+  		method: "POST",
+  		url: "http://localhost:5000/user",
+		contentType: "application/json",
+	 	dataType:"json",
+  		data: JSON.stringify({"timeOnPage": timeOnPage, "furthestScrollPosition":furthestScrollPosition, "clickCount": clickCount})
+	});
+  	request.done(function( msg ) {
+		var responseDBLanding = JSON.stringify(msg);
+  	})
+	request.fail(function( jqXHR, textStatus ) {
+	});
+
+}
+
+function postLogin() {
+        timeOnPage = (new Date()) - pageEntryTime;
+         var request = $.ajax({
+                method: "POST",
+                url: "http://localhost:5000/user/login",
+                contentType: "application/json",
+                dataType:"json",
+                data: JSON.stringify({"timeOnPage": timeOnPage, "furthestScrollPosition":furthestScrollPosition, "clickCount": clickCount})
+        });
+        request.done(function( msg ) {
+		var responseDBLogin = JSON.stringify(msg);
+        })
+        request.fail(function( jqXHR, textStatus ) {
+        });
+
+}
